4ffd1b41073fd4c4750cd9845b652c9cc8218ae6
quochuy1701 <huyhuynh17012000@gmail.com>
2023-02-15 23:39:06 +0700

    code

---
+++ src/redux/reducers/quanLyNguoiDungReducer.js
@@ -0,0 +1,28 @@
+import { userMovie } from "../../utils/setting"
+import { DANG_NHAP } from "../types/quanLyNguoiDungType"
+
+let userLogin = null
+if (localStorage.getItem(userMovie)) {
+  userLogin = JSON.parse(localStorage.getItem(userMovie))
+}
+const initialState = {
+  useLogin: userLogin
+}
+export const quanLyNguoiDungReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case DANG_NHAP:
+      // console.log(action.userLogin)
+      state.useLogin = action.userLogin
+      return { ...state }
+    case "DANG_XUAT":
+      console.log(action.us)
+      state.useLogin = action.usLogin
+      localStorage.removeItem(userMovie);
+      return { ...state }
+
+    default:
+      return state
+  }
+}
+// console.log(initialState.userlogin)
